7b5c79cac29028eb5046c558e052e91e6ebc781f
josias1254 <josiasalmeida1407@gmail.com>
2023-04-03 19:33:26 -0300

    Upload

--- trilhaJS/4arraysEEstruturas/Aula1/Desafio2.js
+++ trilhaJS/4arraysEEstruturas/Aula1/Desafio2.js
@@ -1,10 +1,11 @@
 // 2) Crie um programa que seja capaz de percorrer uma lista de números
 // e imprima cada número Par encontrado.
 
-const lista = [1,2,3,4,5,6,7,8,9,10]
+const lista = [0,1,2,3,4,5,6,7,8,9,10];
 
 for (let i = 0; i < lista.length; i++) {
-    if (lista[i] % 2 === 0 ){
-        console.log(`${lista[i]} é par`)
+    let numeroDaVez = lista[i]
+    if (numeroDaVez % 2 === 0 && numeroDaVez != 0){
+        console.log(`${numeroDaVez} é par`);
     }
 }
