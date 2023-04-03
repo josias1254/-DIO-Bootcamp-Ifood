c992af4d7b680b27720f7cfad2b687b958705f10
josias1254 <josiasalmeida1407@gmail.com>
2023-04-02 22:27:17 -0300

    Atualização

---
+++ trilhaJS/2funcoes/Aula1/Aula.js
@@ -0,0 +1,22 @@
+// Definir função
+function nome(valor) {
+    // Processamento
+    const final = valor + valor
+    // Retorna o valor
+    return final
+
+}
+// Invoca a função
+// console.log(nome(10))
+////////
+
+function calcularJuros() {
+
+}
+
+
+function main() {
+    console.log('Programa principal');
+    calcularJuros();
+}
+main()
