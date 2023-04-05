8514c61a5e848d1f3911cdf79c71e91a3bade83e
josias1254 <josiasalmeida1407@gmail.com>
2023-04-05 12:20:11 -0300

    Upload

--- trilhaJS/4_arrays_estruturas/Teste kk.js
+++ trilhaJS/4_arrays_estruturas/Teste kk.js
@@ -1,47 +1,27 @@
 class Aluno {
-    nome;
-    turma;
-    status = this.status()
-    media = this.calcularMedia()
-    notas;
-
-    constructor(nome, turma, notas) {
-        this.nome = nome;
-        this.turma = turma;
+    constructor(nome, turma) {
+        this.nome = 'nome';
+        this.turma = 'turma';
+        this.notas = [];
         }
 
-   
-    calcularMedia() {
-        let soma = 0;
-        for (let i = 0; i < this.notas.length ; i++) {
-            let nota = this.notas[i];
-            soma = soma + nota;
-        } return soma
-        //const mediaa = soma / notas.length;
-        //return mediaa
-    }
+        calcularMedia() {
+            let soma = 0;
+            let media = [];
+            for (let i = 0; i < this.notas.length ; i++){
+                soma = soma + this.notas[i];
+                media = soma / this.notas.length;
+            }return media
+        }
+        
     status(){
-
     }
 }
 
-const gabriel = new Aluno('gabriel', 'primeiro', [2, 1, 1])
-console.log(gabriel);
 
 
 
-
-
-
-
-/*
-function definirStatus() {
-    if (media >= 7) {
-        return 'Aprovado'
-    } else if (media < 7) { 
-        return 'Reprovado'
-    }
-}
-*/
+const gabriel = new Aluno('gabriel', 'primeiro')
+gabriel.notas.push(3,4,5)
 
 
